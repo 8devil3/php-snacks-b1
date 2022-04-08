c8db34bc7577b6555e69de55f749a8865e0b7a51
8devil3 <8dev.il3@gmail.com>
2022-04-08 15:46:25 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php include './snack-b1.php' ?>
+
+<!DOCTYPE html>
+<html lang="it">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>PHP Snacks B1</title>
+</head>
+
+<body>
+<h1>PHP Snacks B1</h1>
+
+<h2>Snack 1</h2>
+<ul>
+   <?= $partite ?>
+</ul>
+
+<h2>Snack 2</h2>
+<form action="" method="GET" name="access">
+   <input type="text" name="name" placeholder="Nome">
+   <input type="text" name="email" placeholder="Email">
+   <input type="text" name="age" placeholder="Età">
+   <button>invia</button>
+   <?= $strAccess ?>
+</form>
+
+<h2>Sanck 4</h2>
+<p></p>
+
+<h2>snack 5</h2>
+<p></p>
+   
+</body>
+</html>
---
+++ snack-b1.php
@@ -0,0 +1,82 @@
+<?php
+/*
+- SNACK 1 -
+Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema.
+Olimpia Milano - Cantù | 55-60
+*/
+  
+$arrPartite = [
+   [
+      'casa' => [
+         'nome' => 'Olimpia Milano',
+         'punteggio' => '55'
+      ],
+      'ospite' => [
+         'nome' => 'Cantù',
+         'punteggio' => '60'
+      ]
+   ],
+   [
+      'casa' => [
+         'nome' => 'Virtus Bologna',
+         'punteggio' => '24'
+      ],
+      'ospite' => [
+         'nome' => 'Vicenza Basket',
+         'punteggio' => '36'
+      ]
+   ],
+   [
+      'casa' => [
+         'nome' => 'Roma Basket',
+         'punteggio' => '48'
+      ],
+      'ospite' => [
+         'nome' => 'Lazio Basket',
+         'punteggio' => '48'
+      ]
+   ]
+];
+
+$partite = '';
+   
+for ($_i=0; $_i < count($arrPartite); $_i++) {
+   $sqCasa = $arrPartite[$_i]['casa']['nome'];
+   $sqOspite = $arrPartite[$_i]['ospite']['nome'];
+   $punteggioCasa = $arrPartite[$_i]['casa']['punteggio'];
+   $punteggioOspite = $arrPartite[$_i]['ospite']['punteggio'];
+
+   $partite .= "<li>$sqCasa - $sqOspite | $punteggioCasa-$punteggioOspite</li>";
+}
+
+
+
+/*
+- SNACK 2 -
+Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”
+*/
+
+if (isset($_GET['name']) && isset($_GET['email']) && isset($_GET['age'])) {
+   $name = $_GET['name'];
+   $email = $_GET['email'];
+   $age = $_GET['age'];
+   $strAccess = '';
+
+   if (
+         is_string($name) && 
+         strlen($name) > 3 && 
+         strpos($email, '@') && 
+         strpos($email, '.') && 
+         is_numeric($age)
+      ) {
+      $strAccess = "<p>Accesso riuscito</p>";
+   } else {
+      $strAccess = "<p>Accesso negato</p>";
+   }
+}
+
+
+
+
+
+?>
