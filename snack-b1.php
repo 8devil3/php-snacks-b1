b60bbc4c1f0cb0d369d3793a0ac5c4578ef7b7e3
8devil3 <8dev.il3@gmail.com>
2022-04-11 10:48:39 +0200

    corretto snack 5

--- snack-b1.php
+++ snack-b1.php
@@ -91,7 +91,7 @@ $numList = '';
 $rNum = null;
 $max_ran = 150;
 
-for ($x=0; $x < 15; $x++) {
+for ($x=0; $x<15; $x++) {
    
    $rNum = rand(1, $max_ran);
 
@@ -110,9 +110,9 @@ for ($x=0; $x < 15; $x++) {
 Prendere un paragrafo abbastanza lungo, contenente diverse frasi. Prendere il paragrafo e suddividerlo in tanti paragrafi. Ogni punto un nuovo paragrafo.
 */
 
-$p = 'Nel mezzo del cammin di nostra vita mi ritrovai per una selva oscura, ché la diritta via era smarrita. Ahi quanto a dir qual era è cosa dura esta selva selvaggia e aspra e forte che nel pensier rinova la paura. Tant’è amara che poco è più morte; ma per trattar del ben ch’i’ vi trovai, dirò de l’altre cose ch’i’ v’ho scorte. Io non so ben ridir com’i’ v’intrai, tant’era pien di sonno a quel punto che la verace via abbandonai. Ma poi ch’i’ fui al piè d’un colle giunto, là dove terminava quella valle che m’avea di paura il cor compunto, guardai in alto e vidi le sue spalle vestite già de’ raggi del pianeta che mena dritto altrui per ogne calle.';
+$par = 'Nel mezzo del cammin di nostra vita mi ritrovai per una selva oscura, ché la diritta via era smarrita. Ahi quanto a dir qual era è cosa dura esta selva selvaggia e aspra e forte che nel pensier rinova la paura. Tant’è amara che poco è più morte; ma per trattar del ben ch’i’ vi trovai, dirò de l’altre cose ch’i’ v’ho scorte. Io non so ben ridir com’i’ v’intrai, tant’era pien di sonno a quel punto che la verace via abbandonai. Ma poi ch’i’ fui al piè d’un colle giunto, là dove terminava quella valle che m’avea di paura il cor compunto, guardai in alto e vidi le sue spalle vestite già de’ raggi del pianeta che mena dritto altrui per ogne calle.';
 
-$arrP = explode('.', $p);
+$arrP = explode('.', $par);
 
 $newP = '';
 
